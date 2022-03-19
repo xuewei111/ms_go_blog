27c3552d0921869b635fd124af0891721c7912b7
xuewei111 <m13358109269_1@163.com>
2022-03-20 00:02:48 +0800

    配置文件

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -15,27 +13,23 @@ type IndexData struct {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	var indexData IndexData
 	indexData.Title = "go博客"
 	indexData.Desc = "入门教程"
-
-	jsonStr, _ := json.Marshal(indexData)
-	w.Write(jsonStr)
-}
-
-func indexHtml(w http.ResponseWriter, r *http.Request) {
-	var indexData IndexData
-	indexData.Title = "go博客"
-	indexData.Desc = "入门教程"
-
 	t := template.New("index.html")
-	// 获取当前路径
+	// 1.拿到当前的的路径
 	path, _ := os.Getwd()
-	t, _ = t.ParseFiles(path + `/template/index.html`)
+	// 访问博客首页模板的时候,因为有多个模板的嵌套,解析文件的时候,需要将其涉及到的所有模板进行解析
+	home := path + "/template/home.html"
+	header := path + "/template/layout/header.html"
+	footer := path + "/template/layout/footer.html"
+	personal := path + "/template/layout/personal.html"
+	post := path + "/template/layout/post-list.html"
+	pagination := path + "/template/layout/pagination.html"
+	t, _ = t.ParseFiles(path+"/template/ubdex.html", home, header, footer, personal, post, pagination)
 
-	err := t.Execute(w, indexData)
-	fmt.Println(err)
+	// 页面上涉及到的所有的数据,必须有定义
+	t.Execute(w, indexData)
 }
 
 func main() {
@@ -46,7 +40,6 @@ func main() {
 	}
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/index.html", indexHtml)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Print(err)
