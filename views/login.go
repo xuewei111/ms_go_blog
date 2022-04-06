3e85f2e1d0a41ff6811ca48ed777d67649a6d724
xuewei111 <m13358109269_1@163.com>
2022-04-06 21:20:40 +0800

    登录login

---
+++ views/login.go
@@ -0,0 +1,13 @@
+package views
+
+import (
+	"ms-go-blog/common"
+	"ms-go-blog/config"
+	"net/http"
+)
+
+func (*HTMLApi) Login(w http.ResponseWriter, r *http.Request) {
+	login := common.Template.Login
+
+	login.WriteData(w, config.Cfg.Viewer)
+}
